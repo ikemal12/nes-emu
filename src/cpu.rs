8eedf8c985e027cbc2db49ab0021f7940f8dc996
ikemal12 <iltar2005@gmail.com>
2025-03-26 17:04:52 +0000

    Initialise Cargo

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
-    pub register_x: u8;
+    pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
 }
@@ -9,6 +9,7 @@ impl CPU {
     pub fn new() -> Self {
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0,
         }
