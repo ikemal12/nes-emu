df4e6c92571d0926344ad95f8dbbf730ea7bcfbb
Ilter Kemal <iltar2005@gmail.com>
2025-03-26 15:29:02 +0000

    TAX 0xAA instruction added

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,6 @@
 pub struct CPU {
     pub register_a: u8,
+    pub register_x: u8;
     pub status: u8,
     pub program_counter: u16,
 }
@@ -42,6 +43,23 @@ impl CPU {
                 0x00 => {
                     return;
                 }
+
+                0xAA => {
+                    self.register_x = self.register_a;
+
+                    if self.register_x == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_x & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
+
                 _ => todo!()
             }
         }
@@ -66,5 +84,20 @@ mod test {
         let mut cpu = CPU::new();
         cpu.interpret(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b10);
+    }
+
+    #[test]
+    fn test_0xa9_lda_negative_flag() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xff, 0x00]);
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
+    }
+
+    #[test] 
+    fn test_0xaa_tax_move_a_to_x() {
+        let mut cpu = CPU::new();
+        cpu.register_a = 10;
+        cpu.interpret(vec![0xaa, 0x00]);
+        assert_eq!(cpu.register_x, 10);
     }
 }
