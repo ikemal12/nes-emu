b43c4978f6c58ef13c9b53d222b1444fe9596918
ikemal12 <iltar2005@gmail.com>
2025-03-27 15:24:44 +0000

    Implementing memory and reset interrupt

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,6 +3,37 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF]
+}
+
+trait Mem {
+    fn mem_read(&self, addr:u16) -> u8;
+
+    fn mem_write(&mut self, addr:u16, data:u8);
+
+    // Little-Endian addressing
+    fn mem_read_u16(&mut self, pos:u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        (hi << 8) | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos:u16, data:u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+} 
+
+impl Mem for CPU {
+    fn mem_read(&self, addr:u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr:u16, data:u8) {
+        self.memory[addr as usize] = data;
+    }
 }
 
 impl CPU {
@@ -12,6 +43,44 @@ impl CPU {
             register_x: 0,
             status: 0,
             program_counter: 0,
+            memory: [0; 0xFFFF]
+        }
+    }
+
+    fn mem_read(&self, addr:u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr:u16, data:u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run()
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            let opscode = self.mem_read(self.program_counter);
+            self.program_counter += 1;
+
+            match opscode {
+                
+            }
         }
     }
 
@@ -23,6 +92,11 @@ impl CPU {
     fn tax(&mut self) {
         self.register_x = self.register_a;
         self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_x);
     }
 
     fn update_zero_and_negative_flags(&mut self, result:u8) {
@@ -54,6 +128,8 @@ impl CPU {
                 }
 
                 0xAA => self.tax(),
+
+                0xe8 => self.inx(),
 
                 0x00 => return,
 
@@ -96,5 +172,20 @@ mod test {
         cpu.register_a = 10;
         cpu.interpret(vec![0xaa, 0x00]);
         assert_eq!(cpu.register_x, 10);
+    }
+
+    #[test]
+    fn test_5_ops_working_together() {
+        let mut cpu = CPU::new();
+        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        assert_eq!(cpu.register_x, 0xc1);
+    }
+
+    #[test]
+    fn test_inx_overflow() {
+        let mut cpu = CPU::new();
+        cpu.register_x = 0xff;
+        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+        assert_eq!(cpu.register_x, 1);
     }
 }
