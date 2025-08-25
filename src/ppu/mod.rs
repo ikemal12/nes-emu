36da8dd2a802338a9552ad3df175df99fc495ba9
ikemal12 <iltar2005@gmail.com>
2025-08-25 22:26:03 +0100

    add ppu base

---
+++ src/ppu/mod.rs
@@ -0,0 +1,19 @@
+pub struct NesPPU {
+    pub chr_rom: Vec<u8>,
+    pub vram: [u8; 2048],
+    pub oam_data: [u8; 256],
+    pub palette_table: [u8; 32],
+    pub mirroring: Mirroring,
+}
+
+impl NesPPU {
+    pub fn new(chr_rom: Vec<u8>, mirroring: Mirroring) -> Self {
+        NesPPU {
+            chr_rom: chr_rom,
+            mirroring: mirroring,
+            vram: [0; 2048],
+            oam_data: [0; 64 * 4],
+            palette_table: [0; 32],
+        }
+    }
+}
