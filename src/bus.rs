935170e36220d5946d6bb6e63e521737ff8cfe39
ikemal12 <iltar2005@gmail.com>
2025-08-27 20:25:42 +0100

    added final mirror maps for ppu registers

--- src/bus.rs
+++ src/bus.rs
@@ -44,6 +44,8 @@ impl Mem for Bus {
             0x2000 | 0x2001 | 0x2003 | 0x2005 | 0x2006 | 0x4014 => {
                 panic!("Attempt to read from write-only PPU address {:x}", addr);
             }
+            0x2002 => self.ppu.read_status(),
+            0x2004 => self.ppu.read_oam_data(),
             0x2007 => self.ppu.read_data(),
 
             0x2008..=PPU_REGISTERS_MIRRORS_END => {
@@ -63,11 +65,23 @@ impl Mem for Bus {
             RAM ..= RAM_MIRRORS_END => {
                let mirror_down_addr = addr & 0b11111111111;
                self.cpu_vram[mirror_down_addr as usize] = data;
-           }
-           0x2000 => {
-                self.ppu.write_to_ctrl(data);
             }
-
+            0x2000 => {
+                self.ppu.write_to_ctrl(data);
+            } 
+            0x2001 => {
+                self.ppu.write_to_mask(data);
+            }
+            0x2002 => panic!("attempt to write to PPU status register"),
+            0x2003 => {
+                self.ppu.write_to_oam_addr(data);
+            }
+            0x2004 => {
+                self.ppu.write_to_oam_data(data);
+            }
+            0x2005 => {
+                self.ppu.write_to_scroll(data);
+            }
             0x2006 => {
                 self.ppu.write_to_ppu_addr(data);
             }
@@ -79,12 +93,24 @@ impl Mem for Bus {
                 let mirror_down_addr = addr & 0b00100000_00000111;
                 self.mem_write(mirror_down_addr, data);
             }
-           0x8000..=0xFFFF => {
-            panic!("Attempt to write to Cartridge ROM space")
-           }
+           0x8000..=0xFFFF => panic!("Attempt to write to Cartridge ROM space: {:x}", addr),
            _ => {
                println!("Ignoring mem write-access at {}", addr);
            }
         }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::cartridge::test;
+
+    #[test]
+    fn test_mem_read_write_to_ram() {
+        let mut bus = Bus::new(test::test_rom());
+        bus.mem_write(0x01, 0x55);
+        assert_eq!(bus.mem_read(0x01), 0x55);
     }
 }
